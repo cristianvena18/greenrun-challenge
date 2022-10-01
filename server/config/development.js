ca1c16c3fa845cf6400edd2fa00b1574ae2479fa
Wizard <cristiandamianvena@gmail.com>
2022-10-01 11:42:07 -0300

    build: added config folder for library

---
+++ server/config/development.js
@@ -0,0 +1,16 @@
+const development = {
+
+  server: {
+    host: 'localhost',
+    port: 3000,
+  },
+  mysql: {
+    hostname: process.env.MYSQL_HOST,
+    password: process.env.MYSQL_PASSWORD,
+    username: process.env.MYSQL_USERNAME,
+    port: 3306,
+    database: process.env.MYSQL_DATABASE,
+  }
+}
+
+export default development;
